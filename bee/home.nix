d7fb309d5ec8d2c22565cab13adb8a3d4445297b
Chad Russell <chaddouglasrussell@gmail.com>
2025-12-06 15:07:50 -0500

    w

--- bee/home.nix
+++ bee/home.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     dms.homeModules.dankMaterialShell.default
-    quickshell.homeModules.default
+    quickshell.homeManagerModules.default
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
