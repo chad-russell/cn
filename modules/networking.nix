97f14fcf87f7f306be258a58bb47aa6f7b1254ef
Chad Russell <chaddouglasrussell@gmail.com>
2025-12-08 20:03:46 -0500

    weird networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -12,23 +12,21 @@
   config = {
     # Enable systemd-networkd for network management
     systemd.network.enable = true;
-    networking.useDHCP = lib.mkIf (config.customNetworking.staticIP == null) true;
+    networking.useNetworkd = true;
+    networking.useDHCP = false;
     
     # Don't wait for network to be fully configured at boot (prevents timeouts)
-    systemd.services.systemd-networkd-wait-online.serviceConfig.ExecStart = [
-      "" # clear the default
-      "${pkgs.systemd}/lib/systemd/systemd-networkd-wait-online --any"
-    ];
+    systemd.network.wait-online.anyInterface = true;
 
-    # Configure network interface
-    systemd.network.networks."10-lan" = lib.mkIf (config.customNetworking.staticIP != null) {
+    # Configure network interface - either static or DHCP
+    systemd.network.networks."10-lan" = {
       matchConfig.Name = "en*";
-      networkConfig.DHCP = "no";
-      address = [ "${config.customNetworking.staticIP}/24" ];
-      routes = [
+      networkConfig.DHCP = if (config.customNetworking.staticIP == null) then "yes" else "no";
+      address = lib.mkIf (config.customNetworking.staticIP != null) [ "${config.customNetworking.staticIP}/24" ];
+      routes = lib.mkIf (config.customNetworking.staticIP != null) [
         { Gateway = "192.168.20.1"; }
       ];
-      dns = [ "8.8.8.8" "1.1.1.1" ];
+      dns = lib.mkIf (config.customNetworking.staticIP != null) [ "8.8.8.8" "1.1.1.1" ];
     };
 
     # Enable the OpenSSH server
