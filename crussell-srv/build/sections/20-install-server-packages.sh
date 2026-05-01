fed58baed8e9acd11deeb823780ad1cb14579cbe
Chad Russell <chaddouglasrussell@gmail.com>
2026-05-01 09:20:55 -0400

    small updates

--- crussell-srv/build/sections/20-install-server-packages.sh
+++ crussell-srv/build/sections/20-install-server-packages.sh
@@ -6,21 +6,12 @@ echo "::group:: Install Server Packages"
 
 # Core server utilities (not already in base image)
 # Note: htop, jq, unzip, zip, rsync, tmux, tree, wget, curl, smartmontools, nvme-cli, nfs-utils, cifs-utils are in base
-# Note: eza not available in Fedora 43, install via brew if needed
 dnf5 install -y \
     podman-compose \
     restic \
     btop \
     iotop \
     ncdu \
-    du-dust \
-    ripgrep \
-    fd-find \
-    bat \
-    yq \
-    git \
-    neovim \
-    helix \
     lm_sensors \
     hdparm
 
